c1735632a65e74aec03233ed239279f5a0cda59a
TomKo1 <kottomasz98@gmail.com>
2018-03-08 17:21:09 +0100

    1 exercise solved

--- 1/Equation.java
+++ 1/Equation.java
@@ -1,18 +1,49 @@
-package package com.example.tomek.Zadanie1
+//package com.example.tomek.Zadanie1;
+
+import java.lang.Math;
+import java.util.*;
+
+/**
+ *  Class representing quadratic equation and their Real solutions
+ *  @author Tomasz Kot 209362
+ *
+ */
 
 class Equation{
 	private double a,b,c;
-	
-
+    private List<Double> solutions;
+ 
 	Equation(double a, double b, double c){
 		this.a=a;
 		this.b=b;
 		this.c=c;
-	
-
+        this.solutions=new ArrayList<>();
+        solveEquation();
 	}
+    
+
+    
+    private void solveEquation(){
+        
+        double delta = Math.pow(b,2) - 4*a*c;
+        
+        if(delta == 0){
+            double x0 = (-b)/(2*a);
+            solutions.add(x0);
+        }else if(delta > 0){
+            double x1 = (-b-Math.sqrt(delta))/(2*a);
+            double x2 =  (-b+Math.sqrt(delta))/(2*a);
+            solutions.add(x1);
+            solutions.add(x2);
+        }
+        
+    }
 
 
+    @Override
+    public String toString(){
+        return "Equation: "+"("+a+")"+"x^2 + "+"("+b+")"+"x + "+"("+c+")"+"\n"+solutions;
+    }
 
 	
 
--- 1/Main.java
+++ 1/Main.java
@@ -1,46 +1,63 @@
-package package com.example.tomek.Zadanie1
+//package com.example.tomek.Zadanie1;
+
+import java.lang.IllegalArgumentException;
+import java.util.*;
+
+/**
+ *  Class running the program computing solutions of quadratic equations
+ *  @author Tomasz Kot 209362
+ *  @version 1.0
+ *
+ */
+
+//TODO make GUI interface
 
 class Main{
 
 	public static void main(String [] args){
+        
 		List<Double> factorsList = new ArrayList<>();
 		List<Equation> equationsList = new ArrayList<>();
 		int i=0;
 
 		try{
-			if(args.length % 3 != 0 ) throw new IllegalArgumentException("Wrong number of args!!!");
+            
+			if(args.length % 3 != 0 || args.length == 0) throw new IllegalArgumentException("Wrong number of args!!!");
 
 			for(String factor: args){
-				factors.add(Double.parseDouble(factor));
-				if(factors.size() % 3 == 0 ){
-					Equation equation = new Equation(factors.get(i),factors.get(i+1),factor(i+2));
-					i+=3;
-
-					equationsList.add(equation);
+				
+                factorsList.add(Double.parseDouble(factor));
+               
+                if(factorsList.size() % 3 == 0){
+                    
+                
+					
+                    System.out.println(new Equation(factorsList.get(i),factorsList.get(i+1),factorsList.get(i+2)));
+                    
+                    factorsList.clear();
 				}
 			}
 
-			printEquationResult();
-
+		
 		}catch(NumberFormatException e){
-			printHelp();
+            System.out.println("Wrong format of input args");
+            printHelp();
+            System.exit(1);
 		}catch(IllegalArgumentException e){
 			System.out.println(e.getMessage());
+            printHelp();
+            System.exit(2);
 		}
 
 	}
+    
+    
+    
 
-
-	private void printEquationResults(List<Equation> listOfEquations){
-		for(Equation a:listOfEquations){
-			System.out.prontln(a);
-		}
-	}
-
-	private void printHelp(){
-		System.out.println("Wrong format of input args. \n Please 
-			call the program as follows: \n
-			Main a b c where a b c are of type Double\n Please enter factors even when they are 0 \n")
+	public static void printHelp(){
+        System.out.println("---------");
+		System.out.println("Please call the program as follows: \n Main a b c where a b c are of type Double\n Please enter factors even when they are 0 \n");
+        System.out.println("---------");
 	}
 
 
